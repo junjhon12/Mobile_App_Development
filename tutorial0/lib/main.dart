e4b53753c3c17beb42bcd0a2a2ee3b217b52c749
Quoc Bao Dinh Le <qle17@student.gsu.edu>
2024-09-22 12:21:33 -0400

    added plugin

--- tutorial0/lib/main.dart
+++ tutorial0/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:firbase'
 
 void main() {
   runApp(MaterialApp(
@@ -10,14 +11,14 @@ void main() {
   ));
 }
 
-class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
   @override
   State<HomePage> createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
-
   late final TextEditingController _email;
   late final TextEditingController _password;
 
@@ -35,6 +36,7 @@ class _HomePageState extends State<HomePage> {
     super.dispose();
   }
 
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -44,10 +46,15 @@ class _HomePageState extends State<HomePage> {
         children: [
           TextField(
             controller: _email,
-            
+            decoration: const InputDecoration(
+              hintText: 'Enter your email here',
+            ),
           ),
           TextField(
             controller: _password,
+            decoration: const InputDecoration(
+              hintText: 'Enter your password here',
+            ),
           ),
           TextButton(
             onPressed: () async {},
