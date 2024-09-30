6bdd4f1c3f4e7341bda964cc4b37d6f1d16b39aa
Quoc Bao Dinh Le <qle17@student.gsu.edu>
2024-09-30 19:22:30 -0400

    Delete class_activity_6 directory

--- class_activity_6/README.md
+++
@@ -1,3 +0,0 @@
-# class_activity_6
-
-A new Flutter project.
--- class_activity_6/android/app/src/main/kotlin/com/example/class_activity_6/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.class_activity_6
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
