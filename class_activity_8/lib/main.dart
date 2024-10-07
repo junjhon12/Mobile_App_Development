6b2270a79365f2986f02a1567ece7e5cf31d20d5
Quoc Bao Dinh Le <qle17@student.gsu.edu>
2024-10-07 19:02:07 -0400

    Thei mages are moving

--- class_activity_8/lib/main.dart
+++ class_activity_8/lib/main.dart
@@ -1,96 +1,6 @@
 // Tricky Halloween Game: Spooky Surprise
-// Team Members: [junjhon12, anshuk-arun]
-
-import 'package:flutter/material.dart';
-import 'dart:math';
-
-void main() {
-  runApp(TrickyHalloweenGame());
-}
-
-class TrickyHalloweenGame extends StatefulWidget {
-  @override
-  _TrickyHalloweenGameState createState() => _TrickyHalloweenGameState();
-}
-
-class _TrickyHalloweenGameState extends State<TrickyHalloweenGame>
-    with SingleTickerProviderStateMixin {
-  late AnimationController _controller;
-  final List<String> items = ['Bat', 'Ghost', 'Pumpkin', 'Spider', 'Witch'];
-  String? correctItem;
-  String message = '';
-  bool isGameOver = false;
-
-  @override
-  void initState() {
-    super.initState();
-    _controller = AnimationController(
-        vsync: this, duration: Duration(seconds: 2))
-      ..repeat(reverse: true);
-    _selectCorrectItem();
-  }
-
-  void _selectCorrectItem() {
-    correctItem = items[Random().nextInt(items.length)];
-  }
-
-  void _onItemTapped(String item) {
-    if (item == correctItem) {
-      setState(() {
-        message = 'You Found It!';
-        isGameOver = true;
-      });
-    } else {
-      setState(() {
-        message = 'Oh no! It\'s a trap!';
-      });
-    }
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: Text('Tricky Halloween Game')),
-        body: Center(
-          child: Stack(
-            children: items.map((item) {
-              return AnimatedPositioned(
-                duration: Duration(seconds: 2),
-                top: Random().nextDouble() * 400,
-                left: Random().nextDouble() * 400,
-                child: GestureDetector(
-                  onTap: () => _onItemTapped(item),
-                  child: Image.asset(
-                    'assets/$item.jfif', // Adjust the path according to your images
-                    width: 50,
-                    height: 50,
-                  ),
-                ),
-              );
-            }).toList(),
-          ),
-        ),
-        bottomNavigationBar: BottomAppBar(
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Text(
-              message,
-              style: TextStyle(fontSize: 24),
-            ),
-          ),
-=======
-=======
-// Tricky Halloween Game: Spooky Surprise
 // Team Members: [Your Name, Partner's Name]
 
->>>>>>> dd118df (Thei mages are moving)
 import 'package:flutter/material.dart';
 import 'dart:math';
 
@@ -149,12 +59,9 @@ class _TrickyHalloweenGameState extends State<TrickyHalloweenGame>
       home: Scaffold(
         appBar: AppBar(title: Text('Tricky Halloween Game')),
         body: Center(
-<<<<<<< HEAD
-          child: Text('Hello World!'),
->>>>>>> 6b006f3 (created the git and file)
-=======
           child: Stack(
             children: items.map((item) {
+              print('Loading asset: assets/$item.jfif'); // Debugging line
               return AnimatedPositioned(
                 duration: Duration(seconds: 2),
                 top: Random().nextDouble() * 400,
@@ -179,7 +86,6 @@ class _TrickyHalloweenGameState extends State<TrickyHalloweenGame>
               style: TextStyle(fontSize: 24),
             ),
           ),
->>>>>>> dd118df (Thei mages are moving)
         ),
       ),
     );
