1b0d297034fd970f6042625cf7cf434c42a35a99
Quoc Bao Dinh Le <qle17@student.gsu.edu>
2024-11-29 16:21:50 -0500

    firebase setup

--- weatherly/lib/main.dart
+++ weatherly/lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'firebase_options.dart';
 
 void main() {
   runApp(const MainApp());
