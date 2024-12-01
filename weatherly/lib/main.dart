a20f3229f13aaef24075d460e9d7cc424a622c1f
Quoc Bao Dinh Le <qle17@student.gsu.edu>
2024-11-30 19:35:45 -0500

    firebase setup

--- weatherly/lib/main.dart
+++ weatherly/lib/main.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'firebase_options.dart';
+
+
 
 void main() {
   // The main function starts the Flutter app
